94dfb5cada3524fb9e13ba1bbeba7b7b7bd62f3d
shortytapki <v.v.vinogradov99@gmail.com>
2023-11-19 14:13:11 +0300

    fixed app base url

--- src/shared/config/RouteConfig.tsx
+++ src/shared/config/RouteConfig.tsx
@@ -12,9 +12,11 @@ export enum PostRouteParams {
   POST_ID = ':id',
 }
 
+const BASE_URL = import.meta.env.BASE_URL;
+
 export const RoutePaths: Record<AppRoutes, string> = {
-  [AppRoutes.MAIN]: '/',
-  [AppRoutes.POST]: '/post/' + PostRouteParams.POST_ID, // + ID
+  [AppRoutes.MAIN]: BASE_URL,
+  [AppRoutes.POST]: BASE_URL + 'post/' + PostRouteParams.POST_ID, // + ID
   [AppRoutes.NOT_FOUND]: '*',
 };
 
