0f9e3cc31817bb5dd58b9afc111483c4994acd66
shortytapki <v.v.vinogradov99@gmail.com>
2023-11-19 13:56:31 +0300

    added gh-pages workflows

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,37 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { fileURLToPath } from "url";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/virtual-scroll-feed/',
   resolve: {
     alias: [
       {
-        find: "@app",
+        find: '@app',
         replacement: fileURLToPath(
-          new URL("./src/app/App.tsx", import.meta.url)
+          new URL('./src/app/App.tsx', import.meta.url),
         ),
       },
       {
-        find: "@pages",
-        replacement: fileURLToPath(new URL("./src/pages", import.meta.url)),
+        find: '@pages',
+        replacement: fileURLToPath(new URL('./src/pages', import.meta.url)),
       },
       {
-        find: "@widgets",
-        replacement: fileURLToPath(new URL("./src/widgets/", import.meta.url)),
+        find: '@widgets',
+        replacement: fileURLToPath(new URL('./src/widgets/', import.meta.url)),
       },
       {
-        find: "@features",
-        replacement: fileURLToPath(new URL("./src/features", import.meta.url)),
+        find: '@features',
+        replacement: fileURLToPath(new URL('./src/features', import.meta.url)),
       },
       {
-        find: "@entities",
-        replacement: fileURLToPath(new URL("./src/entities", import.meta.url)),
+        find: '@entities',
+        replacement: fileURLToPath(new URL('./src/entities', import.meta.url)),
       },
       {
-        find: "@shared",
-        replacement: fileURLToPath(new URL("./src/shared", import.meta.url)),
+        find: '@shared',
+        replacement: fileURLToPath(new URL('./src/shared', import.meta.url)),
       },
     ],
   },
